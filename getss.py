c25d738832ccdb65874ebb2283e60f4ff2d56ac4
linghutf <xurui788@gmail.com>
2015-09-25 20:27:14 +0800

    更新中国节点
    获取中国用户ss信息(密码经常改变),存储在当前目录下的config.json

---
+++ getss.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+__author__ = 'xdc'
+
+import requests
+import json
+
+def get_ss(url,filename):
+    r =requests.get(url)
+    fp=open(filename,'w')
+    data =r.json()
+
+    #print data
+    data["server"]=data.pop("ip")
+    data["server_port"]=data.pop("port")
+    data["server_port"]=int(data["server_port"])
+    #data["remarks"]="CN_SS"
+    json.dump(data,fp,skipkeys=True)
+    fp.close()
+
+if __name__ =='__main__':
+    get_ss("http://qr.thankgfw.ml/?type=json","config.json")
