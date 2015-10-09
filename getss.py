32408d00134f4c8c3b03e3a4902c5356fd920878
linghutf <xurui788@gmail.com>
2015-10-09 09:02:51 +0800

    fix the problem sslocal can not use the config file
    添加默认部分local_address,local_port,timeout使sslocal识别并正常运行

--- getss.py
+++ getss.py
@@ -12,7 +12,10 @@ def get_ss(url,filename):
     #print data
     data["server"]=data.pop("ip")
     data["server_port"]=data.pop("port")
-    data["server_port"]=int(data["server_port"])
+    data["method"]=data.pop('method')
+    data['local_address']='127.0.0.1'
+    data['timeout']=600
+    data['local_port']='1080'
     #data["remarks"]="CN_SS"
     json.dump(data,fp,skipkeys=True)
     fp.close()
--- us_shadowsocks.py
+++ us_shadowsocks.py
@@ -56,6 +56,8 @@ def getcontent(tag,p):
 
     data={"timeout":600,
           "method":_encry,
+		  "local_port":"1080",
+		  "local_address":"127.0.0.1",
           "password":password,
           "server_port":_port,
           "server":_ip}
